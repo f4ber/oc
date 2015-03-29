cf31a38c6a1bf9ac5900af99d2bab4a305cc0912
f4ber <f4ber@yandex.ru>
2015-03-29 17:29:15 +0500

    update current features
    * remove see all in category menu
    * mail send in contact form patch
    * header and footer modification
    * logo modified
    * fonts added Tahoma Verdana

--- upload/catalog/controller/information/contact.php
+++ upload/catalog/controller/information/contact.php
@@ -12,8 +12,8 @@ class ControllerInformationContact extends Controller {
 
 			$mail = new Mail($this->config->get('config_mail'));
 			$mail->setTo($this->config->get('config_email'));
-			$mail->setFrom($this->request->post['email']);
-			$mail->setSender($this->request->post['name']);
+			$mail->setFrom($this->config->get('config_email'));
+			$mail->setSender($this->request->post['email']);
 			$mail->setSubject(sprintf($this->language->get('email_subject'), $this->request->post['name']));
 			$mail->setText(strip_tags($this->request->post['enquiry']));
 			$mail->send();
